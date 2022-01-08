175bc1f684a02a47f96442e5c1d87e616440c9f6
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-09 00:24:58 +0530

    students and evaluation models are created

---
+++ src/controllers/students.controller.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const reouter = express.Router();
+
+const Student = require("../models/student.model")
--- src/index.js
+++ src/index.js
@@ -3,52 +3,13 @@ const express = require("express");
 const userController = require("./controllers/user.controller");
 
 const connect = require("./configs/db")
-const mongoose =require("mongoose");
-// const connect = ()=>{
-//     return mongoose.connect("mongodb://127.0.0.1:27017/database");
-// }
-
-// const userSchema = new mongoose.Schema({
-//     first_name:{type:String, required:false},
-//     last_name:{type:String,required:false},
-//     email:{type:String, required:false},
-//     gender:{type:String,required:false,default:"male"},
-//     age: {type:Number,required:false},                
-// })
-
-//const User = mongoose.model("user",userSchema);
+//const mongoose =require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/user",userController);
-
-// app.get("/users",async (req,res)=>{
-//     const users = await User.find().lean().exec();
-//     return res.send(users);
-   
-// });
-
-// app.post("/users", async (req, res) => {
-
-//     try {
-  
-//   const users = await User.create(req.body);
-
-//   return res.status(201).send(users)
-
-
-        
-//     } catch (err) {
-//         return res.status(500).send(err.message) 
-//     }
-
-
-
-// })
-
-
 
 app.listen(3456,async ()=>{
    try {
