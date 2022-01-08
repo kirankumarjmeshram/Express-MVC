c85414877de5be5b5bff1526e355932e32ee6bcf
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-08 23:56:03 +0530

    created model and controller for user

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,57 @@
+const express = require("express");
+//const mongoose = require("mongoose");
+
+const router = express.Router();
+
+const User = require("../models/user.model");
+
+router.get("",async (req,res)=>{
+    try{
+        const  users = await User.find().lean()//.exac();
+        return res.send(users)
+    }catch(err){
+        return res.status(500).json({message: err.message,status:"Failed"})
+    }
+    
+})
+
+router.get("/:id",async (req,res)=>{
+    try{
+        const  users = await User.findById(req.params.id).lean()//.exac();
+        return res.send(users)
+    }catch(err){
+        return res.status(500).json({message: err.message,status:"Failed"})
+    }
+    
+})
+
+router.post("",async (req,res)=>{
+    try{
+        const users = await User.create(req.body);
+        return res.status(201).send(users) 
+    }catch(err){
+        return res.status(500).json({messege: err.message,status:"Failed"})
+    }
+
+})
+    
+router.delete("/:id", async (req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id).lean()//.exac();
+        return res.status(200).send(user)
+    }catch(err){
+        return res.status(500).json({message: err.message,status:"Failed"})
+    }
+})
+
+router.patch("/:id",async (req,res)=>{
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean() //.exac();
+        
+    }catch(err){
+        return res.status(500).json({message: err.message,status:"Failed"});
+    }
+
+})
+
+module.exports= router
